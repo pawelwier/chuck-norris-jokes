82d2b92fa29d36c3023e7033ae1942de73d692a3
pawelwier <pawiercinski@gmail.com>
2021-03-27 18:30:30 +0100

    Initial commit, new class, tests

---
+++ src/JokeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pawelwier\ChuckNorrisJokes;
+
+class JokeFactory
+{
+    protected $jokes = [
+        'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+        'Time waits for no man. Unless that man is Chuck Norris.',
+        'If you spell Chuck Norris in Scrabble, you win. Forever.',
+        'Chuck Norris breathes air … five times a day.'
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pawelwier\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Pawelwier\ChuckNorrisJokes\JokeFactory;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $jokes = new JokeFactory([
+            'a test joke',
+        ]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('a test joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $testJokes = [
+            'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+            'Time waits for no man. Unless that man is Chuck Norris.',
+            'If you spell Chuck Norris in Scrabble, you win. Forever.',
+            'Chuck Norris breathes air … five times a day.'
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $testJokes);
+    }
+}
